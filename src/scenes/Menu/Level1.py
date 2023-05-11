ca13af1045f1b8eee56bfdb7dcbcea9a653842f0
Leandro Silva <90656295+Leandro0270@users.noreply.github.com>
2023-05-11 23:33:10 +0000

    Add map structure spawn

--- src/scenes/Menu/Level1.py
+++ src/scenes/Menu/Level1.py
@@ -1,53 +1,43 @@
 import pygame
-
+from tile import Tile
 # Inicializa o pygame
 class Level1:
     def __init__(self):
 
         self.display_surface = pygame.display.get_surface()
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
         self.Level1_MOUSE_POS = pygame.mouse.get_pos()
         self.obstacles_sprites = pygame.sprite.Group()
+        self.LEVEL1_GRIDMAP =[
+            ['x','x','x','x','x','x','x','x','x','x','x','x'],
+            ['x',' ','p',' ','x',' ',' ',' ',' ',' ',' ','x'],
+            ['x',' ',' ',' ','x','e',' ',' ',' ',' ',' ','x'],
+            ['x',' ',' ',' ','x','x',' ','x','x',' ',' ','x'],
+            ['x',' ',' ',' ',' ',' ',' ',' ','x',' ',' ','x'],
+            ['x','x','x','x','x',' ',' ',' ','x',' ',' ','x'],
+            ['x',' ',' ',' ','x',' ',' ',' ','x',' ',' ','x'],
+            ['x',' ',' ',' ','x',' ',' ',' ','x',' ',' ','x'],
+            ['x',' ',' ',' ','x',' ',' ',' ','x',' ',' ','x'],
+            ['x',' ',' ',' ','x','x','x','x','x',' ',' ','x'],
+            ['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+            ['x','x','x','x','x','x','x','x','x','x','x','x']
+        ]
+        self.tileSize = 50;
+        self.generateMap()
 
-def run():
-    pygame.init()
-    pygame.display.set_caption("Level 1")
-
-# Carrega uma tela branca
-    image = pygame.image.load("../../../assets/background/backgroundTest.jpg")
-
-    # Define o tamanho da célula
-    cell_width = 50
-    cell_height = 50
-
-    # Calcula o número de células na grade
-    grid_width = image.get_width() // cell_width
-    grid_height = image.get_height() // cell_height
-
-    # Cria a grade
-    grid = []
-    for i in range(grid_height):
-        row = []
-        for j in range(grid_width):
-            # Calcula a posição e o tamanho do retângulo da célula
-            rect = pygame.Rect(j*cell_width, i*cell_height, cell_width, cell_height)
-            # Obtém a imagem da célula
-            cell_image = image.subsurface(rect)
-            # Adiciona a imagem da célula na linha
-            row.append(cell_image)
-        # Adiciona a linha na grade
-        grid.append(row)
-
-    # Cria uma janela para mostrar a imagem
-    window = pygame.display.set_mode((image.get_width(), image.get_height()))
-
-    # Loop principal
-    running = True
-    while running:
-
-            # Desenha a grade na janela
-        for i, row in enumerate(grid):
-            for j, cell in enumerate(row):
-                window.blit(cell, (j*cell_width, i*cell_height))
-
-            # Atualiza a janela
-        pygame.display.flip()
+    def generateMap(self):
+        for row_index,row in enumerate(self.LEVEL1_GRIDMAP):
+            for col_index,col in enumerate(row):
+                x = col_index * self.tileSize
+                y = row_index * self.tileSize
+                if(col == 'x'):
+                    Tile((x,y),[self.visible_sprites])
+                if(col == 'p'):
+                    print('spawnPlayer')
+                if(col == 'Ev'):
+                    print('spawnEnemy in vertical patrol')
+                if(col == 'EH'):
+                    print('spawnEnemy in horizontal patrol')
+    def run(self):
+        self.visible_sprites.draw(self.display_surface)
