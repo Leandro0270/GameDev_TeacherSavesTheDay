dfd690813def916ec249e4df5c6a0f44693a4986
Leandro Silva <leandro-2703@hotmail.com>
2023-05-22 01:53:02 -0300

    Add ghost behaviour

--- src/scenes/Menu/Level1.py
+++ src/scenes/Menu/Level1.py
@@ -1,5 +1,6 @@
 # Level1.py
 import pygame
+import random
 from Level1Settings import *
 from tile import Tile
 from pathfinding.core.grid import Grid
@@ -9,6 +10,20 @@ from item import Item
 from Ghost import Ghost
 from UI import ui
 from HUD import HUD
+
+
+def find_path(grid_map, start, end):
+    grid = Grid(matrix=grid_map)
+    start = grid.node(start[0], start[1])
+    end = grid.node(end[0], end[1])
+
+    finder = AStarFinder()
+    path, _ = finder.find_path(start, end, grid)
+
+    nodes_path = [(node.x, node.y) for node in path]
+    return nodes_path
+
+
 
 class Level1:
     def __init__(self):
@@ -24,37 +39,40 @@ class Level1:
         self.visible_sprites.add(item)
         self.ui = ui(self.display_surface)
         self.hud = HUD(self.player, len(self.items_sprites), "Chegue ao final da fase", self.display_surface)
+        self.currentGhostPatrolIndex = 0
+
 
 
     def generateMap(self):
-        for row_index,row in enumerate(LEVEL1_GRIDMAP):
-            for col_index,col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if(col == 'x'):
-                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites], col)
-                if(col == 'p'):
-                    print("Instanciou o player")
-                    print(x)
-                    print(y)
-                    self.player = Player((x, y))
-                    print(self.player)
-                    self.visible_sprites.add(self.player)
-                if(col == 'H'):
-                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites], col)
-                if(col == 'es'):
-                    self.ghost = Ghost((x, y), 'ghost.png', 2, LEVEL1_GRIDMAP, TILESIZE)
-                    self.enemies_sprites.add(self.ghost)
-                
+            self.patrol_points =[]
+            for row_index,row in enumerate(LEVEL1_GRIDMAP):
+                for col_index,col in enumerate(row):
+                    x = col_index * TILESIZE
+                    y = row_index * TILESIZE
+                    if(col == 'x'):
+                        Tile((x,y),[self.visible_sprites,self.obstacles_sprites], col)
+                    if(col == 'p'):
+                        self.player = Player((x, y))
+                        self.visible_sprites.add(self.player)
+                    if(col == 'H'):
+                        Tile((x,y),[self.visible_sprites,self.obstacles_sprites], col)
+                    if(col == 'es'):
+                        self.patrol_points.append((x, y))
+                        self.ghost = Ghost((x, y), 2, (x, y))
+                        self.enemies_sprites.add(self.ghost)
+                        self.visible_sprites.add(self.ghost)
+                        
+                    if(col == 'ep'):
+                        self.patrol_points.append((x, y))
+
 
     def run(self):
         running = True
         clock = pygame.time.Clock()
         while running:
             dt = clock.tick(60)
-            grid_data = [[1 if cell == ' ' or cell == 'p' or cell=='es' or cell=='ep' else 0 for cell in row] for row in LEVEL1_GRIDMAP]
+            grid_data = [[0 if cell == 'x' or cell == 'H' else 1 for cell in row] for row in LEVEL1_GRIDMAP]
             grid = Grid(matrix=grid_data)
-
             if self.ui.showing_message:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -83,12 +101,14 @@ class Level1:
                         print("isWalkable")
                         self.player.move_to(pos, grid)
 
-            # Atualizando Ghost
-            if pygame.sprite.collide_rect(self.player, self.ghost):
-                self.player.take_damage(50)
-                self.ghost.reset_position()
 
-            # Verifica se o player colidiu com algum item
+            if(self.ghost.path == []):
+                self.ghost.move_to(self.patrol_points[self.currentGhostPatrolIndex], grid)
+                self.currentGhostPatrolIndex = (self.currentGhostPatrolIndex + 1) % len(self.patrol_points)
+                
+            if self.ghost.handle_collision_with_player(self.player):
+                    self.ghost.path = []          
+
             collided_items = pygame.sprite.spritecollide(self.player, self.items_sprites, True)
             for item in collided_items:
                 self.ui.show_message(item)
@@ -98,8 +118,11 @@ class Level1:
             self.ui.update()
             self.hud.update()
             self.display_surface.blit(self.bg, (0, 0))
-            self.enemies_sprites.draw(self.display_surface)
             self.visible_sprites.draw(self.display_surface)
             pygame.display.update()
 
         return 'MAIN_MENU'
+    
+
+
+        
--- src/scenes/Menu/ghost.py
+++ src/scenes/Menu/ghost.py
@@ -1,54 +1,56 @@
+
+import pygame
+import time
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
-import pygame
 
 class Ghost(pygame.sprite.Sprite):
-    def __init__(self, pos, sprite, speed, grid, tileSize, player):
+    def __init__(self, pos, speed, spawn_point):
         super().__init__()
-        self.player = player
-        self.image = pygame.image.load(sprite)
+        self.image = pygame.image.load('ghost.png').convert_alpha()
         self.rect = self.image.get_rect(center=pos)
-        self.start_pos = pos
-        self.speed = speed
+        self.speed = 90
+        self.spawn_point = spawn_point
         self.path = []
-        self.grid = grid
-        self.tileSize = tileSize
-        self.patrol_points = self.get_patrol_points()
+        self.last_hit_time = 0
+        self.TILESIZE = 33
+        self.current_patrol_point_index = 0
         self.last_update = pygame.time.get_ticks()
 
-    def get_patrol_points(self):
-        patrol_points = []
-        print(self.grid)
-        for row_index, row in enumerate(self.grid):
-            for col_index, col in enumerate(row):
-                if col == 'ep':
-                    patrol_points.append((col_index, row_index))
-        print(patrol_points)
-        return patrol_points
+    def move_to(self, pos, grid):
+        TILESIZE = self.TILESIZE
+        start = grid.node(self.rect.x // TILESIZE, self.rect.y // TILESIZE)
+        end = grid.node(pos[0] // TILESIZE, pos[1]// TILESIZE)
+        finder = AStarFinder()
+        path, runs = finder.find_path(start, end, grid)
+
+        if path:
+            self.path = path[1:]
+
+        if not self.path:  # Verifica se o caminho anterior foi concluído
+            if path:
+                self.path = path[1:]
+        
+
+    def is_near_player(self, player):
+        return abs(self.rect.x - player.rect.x) < 50 and abs(self.rect.y - player.rect.y) < 50
+
+    def handle_collision_with_player(self, player):
+        if pygame.sprite.collide_rect(self, player):
+            current_time = pygame.time.get_ticks() // 1000
+            if current_time - self.last_hit_time >= 5:
+                player.take_damage()
+                self.last_hit_time = current_time
+                self.rect.topleft = self.spawn_point
+                return True
+        return False
+
+        
 
     def update(self):
         now = pygame.time.get_ticks()
-        if not self.path or now - self.last_update > self.speed * 1000:
-            self.path = self.calculate_path()
-            self.last_update = now
-        if self.path and now - self.last_update > self.speed * 10:
+        if self.path and now - self.last_update > self.speed:
             next_pos = self.path[0]
-            self.rect.topleft = (next_pos[0] * self.tileSize, next_pos[1] * self.tileSize)
+            self.rect.topleft = (next_pos[0]*self.TILESIZE, next_pos[1]*self.TILESIZE)
             self.path = self.path[1:]
             self.last_update = now
-
-    def calculate_path(self):
-        grid_data = [[1 if cell == 'x' else 0 for cell in row] for row in self.grid]
-        grid = Grid(matrix=grid_data)
-        path = []
-        start = grid.node(self.rect.x // self.tileSize, self.rect.y // self.tileSize)
-        for patrol_point in self.patrol_points:
-            end = grid.node(patrol_point[0], patrol_point[1])
-            finder = AStarFinder()
-            patrol_path, _ = finder.find_path(start, end, grid)
-            path.extend(patrol_path[1:])  # Exclude the starting point for all paths except the first
-            start = end
-        return path
-
-    def reset_position(self):
-        self.rect.topleft = self.start_pos
